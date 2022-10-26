ca3c54a44d6bb980c3190cd4bab6386a1f965b55
dimausac <usachev2012@gmail.com>
2022-10-26 18:24:39 +0400

    sqlalchemy implementation

---
+++ models/user.py
@@ -0,0 +1,16 @@
+from models.base import Base
+from sqlalchemy import Column, Integer, TIMESTAMP, String, UniqueConstraint
+import datetime
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    __table_args__ = (UniqueConstraint("username", name="username_unique"),)
+
+    id = Column(Integer, primary_key=True)
+    created_at = Column(TIMESTAMP, default=datetime.datetime.utcnow)
+    username = Column(String)
+    short_description = Column(String)
+    long_bio = Column(String)
+
